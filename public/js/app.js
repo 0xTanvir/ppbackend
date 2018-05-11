c2461c0357c2db6608f61c0ed62a045995bc7c6d
Tanvir Rahman <tanvir3080@gmail.com>
2018-05-12 05:30:59 +0600

    contest create dialog added

--- public/js/app.js
+++ public/js/app.js
@@ -2,23 +2,43 @@
  * Created by Tanvir on 2018-04-27.
  */
 
- // For registration or join
- $(function (){
-     var $fname = $("#firstname");
-     var $lname = $("#lastname");
-     var $email = $("#email");
-     var $password = $("#password");
-     var $contactno = $("#contactno");
-     var $vjudge = $("#vjudge");
+$(function () {
 
-     $("#btn-sign-up").on('click', function() {
+    $( "#cCreateButton" ).click(function(){
+        $( "#cCreateDialog" ).dialog("open")
+    });
+
+    $( "#cCreateDialog" ).dialog({
+        draggable:false,
+        resizable:false,
+        closeOnEscape:false,
+        modal:true,
+        autoOpen:false,
+        minWidth:585
+    });
+    $(".ui-dialog-titlebar").hide();
+    
+    $( "#cCreateClear" ).click(function(){
+        $( "#cCreateDialog" ).dialog("close")
+    });
+
+    
+    // For registration or join
+    var $fname = $("#firstname");
+    var $lname = $("#lastname");
+    var $email = $("#email");
+    var $password = $("#password");
+    var $contactno = $("#contactno");
+    var $vjudge = $("#vjudge");
+
+    $("#btn-sign-up").on('click', function () {
         var user = {
-            firstname : $fname.val(),
-            lastname : $lname.val(),
-            email : $email.val(),
-            password : $password.val(),
-            contactno : $contactno.val(),
-            vjudge : $vjudge.val(),
+            firstname: $fname.val(),
+            lastname: $lname.val(),
+            email: $email.val(),
+            password: $password.val(),
+            contactno: $contactno.val(),
+            vjudge: $vjudge.val(),
         };
 
         $.ajax({
@@ -34,36 +54,34 @@
                 }
             }
         });
-     });
-
-
-    
- });
+    });
 
 
 
- $(function (){
+    // Handle blog create button
     var $tittle = $("#tittle");
     var $description = $("#description");
 
-    $("#btn-post").on('click', function() {
-       var pst = {
-           tittle : $tittle.val(),
-           description : $description.val(),  
-       };
+    $("#btn-post").on('click', function () {
+        var pst = {
+            tittle: $tittle.val(),
+            description: $description.val(),
+        };
 
-       $.ajax({
-           url: '/blog/create',
-           type: 'POST',
-           data: JSON.stringify(pst),
-           dataType: "json",
-           contentType: "application/json",
-           success: function (response) {
-               // redirect must be defined and must be true
-               if (response.redirect !== undefined && response.redirect) {
-                   window.location.href = response.redirectUrl;
-               }
-           }
-       });
+        $.ajax({
+            url: '/blog/create',
+            type: 'POST',
+            data: JSON.stringify(pst),
+            dataType: "json",
+            contentType: "application/json",
+            success: function (response) {
+                // redirect must be defined and must be true
+                if (response.redirect !== undefined && response.redirect) {
+                    window.location.href = response.redirectUrl;
+                }
+            }
+        });
     });
+
+
 });
